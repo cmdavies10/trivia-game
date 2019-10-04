bc55e10bf03c80d6d781af39f99fb3aed6ed1188
Chris Davies <c.m.davies10@gmail.com>
2019-10-04 07:25:49 -0400

    updated main pages with initial build

---
+++ assets/javaScript/app.js
@@ -0,0 +1,19 @@
+// add document ready function
+//create variables
+var questionArray = [
+    {},
+    {},
+    {},
+];
+
+// create starting page on load
+var a = $("<button>");
+a.text("Start!");
+a.addClass("btn");
+$(".timer").append(a);
+
+// once starting button clicked, start game - load = timer 30 seconds, question and four answer choices
+$(".btn").on("click", function () {
+    
+    
+});
