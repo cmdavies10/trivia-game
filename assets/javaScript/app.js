392fbb813d246a82a479f9ccf4e3bb5da7356736
Chris Davies <c.m.davies10@gmail.com>
2019-10-04 15:49:49 -0400

    working start page and first question

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,63 +1,144 @@
 // add document ready function
 //create variables
-var questionArray = {
-    questions: [
-        "What time is it?",
+var questionArray = [
+    {
+        question: "What time is it?",
+        answers: [
+            "Four",
+            "Midnight",
+            "High Noon",
+            "Game Time"
     ],
-    answers: [
-        "Game time",
-    ]
-};
+        correctAnswer: "Game Time"
+    },
+    {
+        question: "What color is the sky",
+        answers: {
+            a: "Blue",
+            b: "Red",
+            c: "Green",
+            d: "Up"
+        },
+        correctAnswer: "Blue"
+    }
+];
+//     questions: [
+//         "What time is it?",
+//         "What color is the sky",
+//     ],
+//     answers: [
+//         "Game time",
+//         "Blue",
+//     ]
+// };
 
-console.log(questionArray.questions[0]);
-console.log(questionArray.answers[0]);
+var answersFirst = ["Four", "Midnight", "High Noon", "Game time"];
+var answersSecond = ["Blue", "Red", "Green", "Up"]
+
+console.log(questionArray[0].answers);
+
+
+console.log(questionArray[0].answers.b);
+// console.log(questionArray);
+// console.log(questionArray.answers[0]);
 
 var userGuess = [];
 var countdown = 30;
-var interalID = setInterval(count, 1 * 1000);
+var clockRunning = false;
+var numCorrect = 0;
+var numWrong = 0;
 
 
 // create starting page on load
 window.onload = function() {
     var a = $("<button>");
     a.text("Start!");
-    a.addClass("btn btn-lg start-button");
+    a.addClass("btn start-button");
     $(".question").append(a);
     $(".start-button").on("click", start);
+    
+    console.log(questionArray[0].answers);
+
 };
 
-function start() {
-    countdown = 30;
-    $(".timer").text("Time Remaining: " + countdown);
-    count();
+// click an answer choice --> displays gif of correct answer, changes results, generates new question + answer choices  
+$(".choices").on("click", function (event) {
+    event.preventDefault();
+    var userGuess = $(this).val();
+    // console.log($(this).val());
+})
 
-    for (var i = 0; i < questionArray; i++) {
-        $(".question").text(questionArray.questions[0]);
-        $(".answers").text(questionArray.answers[i]);
+// function that starts the game by pressing the start button
+function start() {
+    if (!clockRunning) {
+        interalID = setInterval(count, 1 * 1000);
+        clockRunning = true;
     }
-    
-    
-    
+    // resetClock();
+    // countdown = 30;
+    // $(".timer").text("Time Remaining: " + countdown);
+    // count();
+
+    // question 1:
+    $(".question").text(questionArray[0].question);
+    // answer choices 1:
+    for (var i = 0; i < answersFirst.length; i++) {
+        console.log(answersFirst);
+        var b = $("<button>");
+        b.text(answersFirst[i]);
+        b.addClass("choices")
+        $(".answers").append(b);
+    };
+
+    // question 2:
+    // if clock = zero OR user chooses an anwer, then --> display gif of correct answer, update score wins/losses, generate next question
+    if (countdown === 0) {
+        resetClock();
+        $(".question").text(questionArray[1].question);
+        for (var i = 0; i < answersSecond.length; i++) {
+            var b = $("<button>");
+            b.text(answersSecond[i]);
+            b.addClass("choices")
+            $(".answers").append(b);
+        };
+    }
+
+
+    // first range of answers:
+    // for (var i = 0; i < answersFirst.length; i++) {
+    //     var b = ("<button>");
+    //     b.addClass("btn btn-lg choices");
+    //     b.attr("data-name", answersFirst[i]);
+    //     b.text(answersFirst[i]);
+    //     $(".answers").append(b);
+    // }
+
+    //     $(".answers").text(questionArray.answers[i]);
+    // };
 };
 
 function resetClock() {
+    // set clock to 30 seconds
     countdown = 30;
-    $(".timer").text("Time Remaining: " + countdown);
+
     count();
 };
-console.log(resetClock());
-resetClock();
-
 
 function count() {
+     // show/change the current time display
+    $(".timer").text("Time Remaining: " + countdown);
     // decrement the countdown variable by 1
     countdown--;
-    // change the current time display
-    
-};
- 
-// once starting button clicked, start game - load = timer 30 seconds, question and four answer choices
-// $(".btn").on("click", function () {
+   
     
 
-// });
+    if (countdown === 0) {
+        stop();
+        resetClock();
+    }
+};
+
+function stop() {
+    clearInterval(interalID);
+    clockRunning = false;
+}
